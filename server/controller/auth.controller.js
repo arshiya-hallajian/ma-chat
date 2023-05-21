a916941f5f5f4fce4a4f71d90f279b397dd7ccc6
elliot sad <bamdadmahdi05@gmail.com>
2023-05-21 23:30:47 +0200

    auth controller :  user login & add token

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,26 +1,81 @@
-// const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcrypt");
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-
-//user login
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const Token = require("../models/token");
+const crypto = require("crypto");
+//prisma connection
+// const { PrismaClient } = require("@prisma/client");
+// const prisma = new PrismaClient();
+const Crypto = require("crypto");
+// user login
 module.exports.login = async (req, res) => {
-   
-const email = req.body.email;
-  try { 
-    //find user in database
-    const user = await prisma.user.findUnique({
-      where: {
-        email : email
-      }
-    })
+  const email = req.body.email;
 
+  try {
+    //find user in database
+    const user = await User.findOne({ email: email });
     //if user is not found
     if (!user) {
-      return res.status(404).json({ success: false, message: "User not found" });
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
     }
-  
+
+    // check the password and compare the password
+    const checkPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!checkPassword) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Password wrong" });
+    }
+    //add jsonwebtoken
+    const token = jwt.sign(
+      {
+        id: user._id,
+        username: user.firstname,
+        email: user.email,
+      },
+      "MySecretKey",
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    //add token to the collection
+    await Token.findOneAndUpdate(
+      {
+        _userId: user._id,
+        tokenType: "login",
+      },
+      {
+        token,
+      },
+      {
+        new: true,
+        upsert: true,
+      }
+    );
+
+    // send response
+    res.status(200).json({
+      status: true,
+      data: {
+        token,
+        user,
+      },
+      message: "kir to programming",
+      error: undefined,
+    });
   } catch (err) {
-    res.status(404).json({status:false, data: err, message: "error in database connection"});
+    res.status(404).json({
+      status: false,
+      data: undefined,
+      message: "error in database connecting",
+      error: err,
+    });
   }
 };
+
