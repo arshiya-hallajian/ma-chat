0f32d38f74b51b49740e98fcb3fd49f7e5c6c68a
elliot sad <bamdadmahdi05@gmail.com>
2023-05-21 23:42:47 +0200

    auth controller : user list & forget password& ...

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -155,3 +155,103 @@ module.exports.logoutAll = async (req, res) => {
   }
 };
 
+module.exports.userList = async (req, res) => {
+  try {
+    const users = await User.findOne({}).exec();
+    res.status(200).json({
+      status: true,
+      data: users,
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: false,
+      data: e,
+    });
+  }
+};
+
+//forget password controller
+module.exports.forgetPassword = async (req, res) => {
+  //find user in database
+  const user = await User.findOne({ email: req.body.email }).exec();
+  if (!user) return res.status(400).send("email not found");
+
+  //find token and create hash
+  await Token.findOneAndDelete({ _userId: user._id });
+  const cryptedToken = crypto.randomBytes(32).toString("hex");
+  const hash = await bcrypt.hash(cryptedToken, 10);
+
+  //create new token model
+  const tokenModel = new Token({
+    token: hash,
+    _userId: user._id,
+    tokenType: "forgetPassword",
+  });
+  //send token in database
+  await tokenModel.save();
+  //send link with email
+  const link = `localhost:4001/api/auth/resetPassword/?userid=${user._id}&token=${cryptedToken}`;
+
+  await sendMail(user.email, "password reset", link);
+  res.status(200).json({
+    status: true,
+    message: "email sended",
+  });
+};
+
+module.exports.resetPassword = async (req, res) => {
+  const token = req.query.token;
+  const userId = req.query.userid;
+  const password = req.body.password;
+
+  //check token in TokenCollection
+  const resetPasswordToken = await Token.findOne({ _userId: userId });
+  if (!resetPasswordToken) {
+    return res.status(400).json({
+      status: false,
+      message: "Invalid or Expired Token",
+    });
+  }
+
+  //compare hashed pass and token
+  const checkToken = await bcrypt.compare(token, resetPasswordToken.token);
+  if (!checkToken) {
+    return res.status(400).json({
+      status: false,
+      message: "Invalid or Expired Token",
+    });
+  }
+
+  //hash new password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  //change user password
+  const changeUserPassword = await User.updateOne(
+    { _id: userId },
+    { $set: { password: hashedPassword } },
+    { new: true }
+  );
+  if (!changeUserPassword) {
+    return res.status(400).json({
+      status: false,
+      message: "database error",
+    });
+  }
+
+  //take user data from database
+  const user = await User.findById(resetPasswordToken._userId).exec();
+  if (!user) {
+    return res.status(400).json({
+      status: false,
+      message: "database error",
+    });
+  }
+  //take user data from database
+  const removeToken = await Token.findByIdAndRemove(user._id);
+  if (!removeToken) {
+    return res.status(400).json({
+      status: false,
+      message: "database error",
+    });
+  }
+};
