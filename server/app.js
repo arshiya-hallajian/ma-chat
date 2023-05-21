965a72e1a60bd4eb4d412976a5d6523505ef1003
elliot sad <bamdadmahdi05@gmail.com>
2023-05-21 23:45:23 +0200

    update app.js  02

--- server/app.js
+++ server/app.js
@@ -1,28 +1,34 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = 3001;
+const port = 3001;
+const mongoose = require("mongoose");
+const env = require("dotenv").config();
 
-
-//imported routers
+//routes
 const auth_router = require("./routes/auth.router");
+// const product_router = require('./routes/product.router');
+// const cart_router = require('./routes/cart.router')
 
-//middlewares
+//middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//database connect
+mongoose.connect(process.env.ADMIN_ID).then(() => {
+  console.log("connect to db");
+});
 
-//routers
+//use routers
 app.use("/api/auth", auth_router);
-app.get("/api/auth/login",async (req, res) => {
-  res.send("welcome in the backend!");
- 
+// app.use("/api/product", product_router);
+// app.use("/api/cart", cart_router);
+
+app.get("/", (req, res) => {
+  res.send("Welcome Shine Backend");
 });
 
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(port, () => console.log(`listening on port ${port}!`));
 
 module.exports = app;
